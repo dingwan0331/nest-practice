a6647648a4b33bf90b2ad3e7e07fa9664d2c1fe5
dingwan <dingwan0331@gmail.com>
2022-10-04 23:10:16 +0900

    feat: boards modules 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 
+import { BoardsModule } from './boards/boards.module';
+
 @Module({
-  imports: [],
+  imports: [BoardsModule],
 })
 export class AppModule {}
