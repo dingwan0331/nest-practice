39a5c44c33b02f13aad488aa4254df1c021b9005
dingwan <dingwan0331@gmail.com>
2022-10-05 14:09:16 +0900

    feat: 게시물작성 기능 구현
    - controller & service 구현
    - uuid 모듈 설치

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './boards.model';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ): Board {
+    return this.boardsService.createBoard(title, content);
+  }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = [];
+
+  createBoard(title: string, content: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      content,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
+  }
+}
