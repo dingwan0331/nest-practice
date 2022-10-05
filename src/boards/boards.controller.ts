7a5c5790cd85cae281f722f57d1f0f9be74e51ef
dingwan <dingwan0331@gmail.com>
2022-10-05 14:30:16 +0900

    feat: 게시물작성 기능 Dto 적용

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,16 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './boards.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
   @Post()
-  createBoard(
-    @Body('title') title: string,
-    @Body('content') content: string,
-  ): Board {
-    return this.boardsService.createBoard(title, content);
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
 
-  createBoard(title: string, content: string) {
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    const { title, content } = createBoardDto;
     const board: Board = {
       id: uuid(),
       title,
