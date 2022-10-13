2254030130bb9540d76afa50e4a0b4af976dcf44
dingwan <dingwan0331@gmail.com>
2022-10-13 17:42:24 +0900

    fix: typeorm.config.ts 수정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,14 +1,17 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const env = process.env;
 
-export const typeORMConfig: TypeOrmModule = {
+export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: env.MYSQL_HOST,
   port: 3306,
   username: env.MYSQL_USERNAME,
   password: env.MYSQL_PASSWORD,
   database: env.MYSQL_DATABASE,
-  entities: [],
-  synchronize: true,
+  entities: ['dist/**/*.entity.{js,ts}'],
+  synchronize: Boolean(env.MYSQL_SYNC),
 };
