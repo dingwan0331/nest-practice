10eea7ceff7c90215c330528ae87efa51f768671
dingwan <dingwan0331@gmail.com>
2022-10-13 14:55:30 +0900

    refactor: board-models 수정
    - board interface 삭제
    - 파일명 board-status-enum으로 변

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './boards.model';
+import { Board, BoardStatus } from './board-status-enum';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
 
