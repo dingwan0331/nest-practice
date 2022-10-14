4de4cd5dd8e400f5b2399676c87d3ccb63feeee9
dingwan <dingwan0331@gmail.com>
2022-10-14 22:34:35 +0900

    refactor: 게시물 생성 기능

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,5 +1,19 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { BoardStatus } from './board-status-enum';
 import { Board } from './board.entity';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @EntityRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+export class BoardRepository extends Repository<Board> {
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    const { title, content } = createBoardDto;
+
+    const board = this.create({
+      title: title,
+      content: content,
+      status: BoardStatus.PUBLIC,
+    });
+
+    return await this.save(board);
+  }
+}
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,16 +2,22 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { BoardRepository } from './board.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Board } from './board.entity';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
   constructor(
-    @InjectRepository(Board)
+    @InjectRepository(BoardRepository)
     private boardRepository: BoardRepository,
   ) {}
 
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    return await this.boardRepository.createBoard(createBoardDto);
+  }
+
   async getBoardById(id: number): Promise<Board> {
     const found = await this.boardRepository.findOne(id);
+
     if (!found) {
       throw new NotFoundException();
     }
