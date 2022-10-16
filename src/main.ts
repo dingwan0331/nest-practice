158295df8256cc110ced83ffac0b37bc624fa890
dingwan <dingwan0331@gmail.com>
2022-10-16 19:59:23 +0900

    refactor: main.ts dotenv 제거

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import { SERVER_PORT } from './configs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
