2740816d9ab20613de105e4cc0d7fa6037e06243
dingwan <dingwan0331@gmail.com>
2022-10-16 17:36:50 +0900

    refactor: 회원가입 ValidationPipe 추가

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,14 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(15)
   username: string;
+
+  @IsString()
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/, {
+    message: 'Invalid Password',
+  })
   password: string;
 }
